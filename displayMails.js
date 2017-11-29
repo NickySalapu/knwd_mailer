104a85d99dbff5a3438dfe533ac2e03506091c34
Piotr Dabrowski <pd96loss@gmail.com>
2017-11-29 20:12:23 +0100

    Auto stash before merge of "master" and "origin/master"

--- displayMails.js
+++ displayMails.js
@@ -1,11 +1,57 @@
 class displayMails {
 
+<<<<<<< Updated upstream
     displayOneMail(mailFrom2, mailTo, subject) {
+=======
+    displayOneMail(mailFrom, mailTo, subject) {
+      const sqlite3 = require('sqlite3').verbose();
+      let db = new sqlite3.Database('../db/knwd_mailer.db');
+>>>>>>> Stashed changes
+
+      let sql = `SELECT mailFrom, mailTo, subject FROM mailList WHERE mailFrom = ? AND mailTo = ? AND subject = ?`;
+
+        db.each(sql, [mailFrom, mailTo, subject], (err, row) => {
+
+          if (err) {
+            throw err;
+          }
+          console.log(`${row.mailFrom} - ${row.mailTo} : ${row.subject}`);
+
+      });
+
+      db.close();
 
     }
 
-    displayMailsFromFile() {
-
-    }
-
+<<<<<<< Updated upstream
 }
+=======
+    displayMailsFromFile(mailToList) {
+
+        var fs = require('fs');
+        //var mailToList = fs.readFileSync(mailToListSplit, 'UTF-8');
+        fs.readFile(`./${mailToList}`, function(text){
+        var mailToListSplit = text.split("\n")
+        });
+
+        let sql2 = `SELECT mailFrom, mailTo, subject, data FROM mailList WHERE mailTo = ?`;
+
+        //Czy foreach zadziala na pliku? Bo chyba tylko na array
+        mailToListSplit.foreach(function(index){
+
+            db.each(sql2, [index], (err, row) => {
+
+              if (err) {
+                throw err;
+              }
+              console.log(`${row.mailFrom} - ${row.mailTo} : ${row.subject} - ${row.data}`);
+          });
+    });
+
+        db.close();
+
+    }
+}
+
+module.exports = displayMails;
+>>>>>>> Stashed changes
