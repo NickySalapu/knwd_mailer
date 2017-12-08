5486d8cb896e84cb466f490c25d19e91d4a46523
Damian <blinkinruzdecaber@gmail.com>
2017-12-08 20:03:08 +0100

    knwd_mailer_#3
    displayed body email in a terminal

--- SendMail.js
+++ SendMail.js
@@ -11,12 +11,16 @@ class sendMail {
                 pass: 'verysecret'
             }
         });
+        let id = Math.floor((Math.random() * 100000) + 1); //random id for email
         let mailOptions = {
             from: configuration.mailFrom,
             to: 'bar@blurdybloop.com, baz@blurdybloop.com',
             subject: "Hello",
             text: 'Hello world?',
-            html: '<b>Hello world?</b>'
+            html: '<b>Hello world?</b>',
+            //added fake img to email - it will be check if email was opened
+            //maybe link should be added to email body
+            img: '<img src="http://mail.pietruszka.usermd.net/api/check/'+id+'">'
         };
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
