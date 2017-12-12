61603255a1ae4e995476f5adbfc2ecfcfbb3162c
Damian <blinkinruzdecaber@gmail.com>
2017-12-12 23:25:33 +0100

    knwd_mailer_openedMail#5
    Added loop to file

--- openedMails.js
+++ openedMails.js
@@ -6,55 +6,44 @@ class openedMails {
 
   function ifOpened() {
 
-    //let id = db.run("SELECT id_mailTo FROM mailList");
+    // let id = db.run("SELECT id_mailTo FROM mailList");
     let clicked = '';
 
 ////////////////////////////////////////////////////
+    var id_arr = [];
+    var j = 0;
     db.all("SELECT id_mailTo FROM mailList", function(err, rows) {
-        rows.forEach(function (row) {
-          request({
-            method: 'POST',
-            url: 'http://mail.pietruszka.usermd.net/api/check',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: "{  \"id\": \"" + id + "\"}"
-          }, function(error, response, body) {
-            // console.log('Status:', response.statusCode);
-            // console.log('Headers:', JSON.stringify(response.headers));
+      rows.forEach(function(row) {
+        id_arr[j] = row.id_mailTo;
+        i++
+      })
+    });
+//////////////////////////////////////////////////
 
-            var parsed = JSON.parse(body);
-            var body_arr = [];
+    for (var i = 0; i < id_arr.length; i++) {
+      request({
+        method: 'POST',
+        url: 'http://mail.pietruszka.usermd.net/api/check',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: "{  \"id\": \"" + id_arr[i] + "\"}"
+      }, function(error, response, body) {
+        // console.log('Status:', response.statusCode);
+        // console.log('Headers:', JSON.stringify(response.headers));
 
-            for (var x in parsed) {
-              body_arr.push(parsed[x]);
-            }
+        var parsed = JSON.parse(body);
+        var body_arr = [];
 
-            console.log('Opened (true/false):', row.body_arr[0]);
-          });
-        })
-    })
-////////////////////////////////////////////////////
-    // request({
-    //   method: 'POST',
-    //   url: 'http://mail.pietruszka.usermd.net/api/check',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: "{  \"id\": \"" + id + "\"}"
-    // }, function(error, response, body) {
-    //   // console.log('Status:', response.statusCode);
-    //   // console.log('Headers:', JSON.stringify(response.headers));
-    //
-    //   var parsed = JSON.parse(body);
-    //   var body_arr = [];
-    //
-    //   for (var x in parsed) {
-    //     body_arr.push(parsed[x]);
-    //   }
-    //
-    //   console.log('Opened (true/false):', body_arr[0]);
-    // });
+        for (var x in parsed) {
+          body_arr.push(parsed[x]);
+        }
+
+        console.log('Opened (true/false):', body_arr[0]);
+      });
+    }
+
+//////////////////////////////////////////////////////////////
   }
 
 }
